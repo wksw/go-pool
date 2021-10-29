84c89b9640822d3370599ad8755356fb53737564
wksw <2223721904@qq.com>
2021-10-29 16:48:50 +0800

    add test

--- pipeline_test.go
+++ pipeline_test.go
@@ -1,6 +1,9 @@
 package gopool
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestPipelineGraph(t *testing.T) {
 	job1 := NewJob("job1", &testJob{Name: "job1"})
@@ -24,4 +27,51 @@ func TestPipelineGraph(t *testing.T) {
 		t.FailNow()
 	}
 	t.Log(graph)
+}
+
+func next(self *Job) bool {
+	for _, job := range self.GetUpstreams() {
+		if job.GetStatus() != JobSuccess {
+			return false
+		}
+	}
+	return true
+}
+
+func TestPipeline(t *testing.T) {
+	pool := NewPool(10, 5).WithEventCallback(EventLevelDebug, func(event *Event) {
+		t.Log(event)
+	}).WithExitCallback(func(reason string) {
+		t.Log(reason)
+	})
+	nameSpaceJob := NewJob("nameSpaceJob", &testJob{Name: "nameSpaceJob"})
+	configmapJob := NewJob("configmapJob", &testJob{Name: "configmapJob"}).When(next)
+	secretJob := NewJob("secretJob", &testJob{Name: "secretJob"}).When(next)
+	deploymentJob := NewJob("deploymentJob", &testJob{Name: "deploymentJob"}).When(next)
+	serviceJob := NewJob("serviceJob", &testJob{Name: "serviceJob"}).When(next)
+	autoscaleJob := NewJob("autoscaleJob", &testJob{Name: "autoscaleJob"}).When(next).WithResultCallback(func(result interface{}, err error) {
+		pool.Close("finish")
+	})
+	if err := nameSpaceJob.Before(configmapJob, secretJob); err != nil {
+		t.Error(err.Error())
+		t.FailNow()
+	}
+
+	if err := deploymentJob.After(configmapJob, secretJob); err != nil {
+		t.Error(err.Error())
+		t.FailNow()
+	}
+
+	if err := deploymentJob.Before(serviceJob, autoscaleJob); err != nil {
+		t.Error(err.Error())
+		t.FailNow()
+	}
+	pipeline, err := NewPipeline("create", nameSpaceJob, configmapJob, secretJob, deploymentJob, serviceJob, autoscaleJob)
+	if err != nil {
+		t.Error(err.Error())
+		t.FailNow()
+	}
+
+	pool.AddPipeline(pipeline)
+	time.Sleep(time.Second)
 }
